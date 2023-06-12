059ff7cf5226edb8f21e62fae18a95909fccdb18
Geundol222 <geundol222@gmail.com>
2023-06-12 21:49:41 +0900

    feat : HomeWork0612

---
+++ Assets/HomWork/2023.06.12/Scripts/PlayerMover.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace HomeWork0612
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        [SerializeField] float walkSpeed;
+        [SerializeField] float runSpeed;
+        [SerializeField] float jumpSpeed;
+
+        private Animator anim;
+        private CharacterController controller;
+        private Vector3 moveDir;
+        private float curSpeed;
+        private float ySpeed;
+        private bool isGrounded;
+        private bool isWalk;
+
+        private void Awake()
+        {
+            controller = GetComponent<CharacterController>();
+            anim = GetComponent<Animator>();
+        }
+
+        private void OnEnable()
+        {
+            StartCoroutine(MoveRoutine());
+        }
+
+        IEnumerator MoveRoutine()
+        {
+            while (true) 
+            {
+                Move();
+                Fall();
+                yield return null;
+            }
+        }
+
+        private void Move()
+        {
+            if (moveDir.magnitude == 0)
+            {
+                curSpeed = Mathf.Lerp(curSpeed, 0, 0.05f);
+                anim.SetFloat("MoveSpeed", curSpeed);
+                return;
+            }
+
+            if (isWalk)
+            {
+                curSpeed = Mathf.Lerp(curSpeed, walkSpeed, 0.05f);
+                anim.SetFloat("MoveSpeed", curSpeed);
+            }
+            else
+            {
+                curSpeed = Mathf.Lerp(curSpeed, runSpeed, 0.05f);
+                anim.SetFloat("MoveSpeed", curSpeed);
+            }
+
+            Transform camera = Camera.main.transform;
+            Vector3 forwardVector = new Vector3(camera.forward.x, 0, camera.forward.z).normalized;
+            Vector3 rightVector = new Vector3(camera.right.x, 0, camera.right.z).normalized;
+
+            controller.Move(forwardVector * moveDir.z * curSpeed * Time.deltaTime);
+            controller.Move(rightVector * moveDir.x * curSpeed * Time.deltaTime);
+
+            Quaternion lookRotation = Quaternion.LookRotation(forwardVector * moveDir.z + rightVector * moveDir.x);
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 0.05f);
+        }
+
+        private void OnMove(InputValue value)
+        {
+            moveDir.x = value.Get<Vector2>().x;
+            moveDir.z = value.Get<Vector2>().y;
+        }
+
+        private void Fall()
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+
+            if (isGrounded && ySpeed < 0)
+                ySpeed = 0;
+            
+            controller.Move(Vector3.up * ySpeed * Time.deltaTime);
+        }
+
+        private void Jump()
+        {
+            ySpeed = jumpSpeed;
+        }
+
+        private void OnJump(InputValue value)
+        {
+            Jump();
+        }
+
+        private void OnWalk(InputValue value)
+        {
+            isWalk = value.isPressed;
+        }
+
+        private void GroundChecker()
+        {
+            RaycastHit hit;
+
+            isGrounded = Physics.SphereCast(transform.position + Vector3.up * 1f, 0.4f, Vector3.down, out hit, 0.6f);
+        }
+
+        private void OnDisable()
+        {
+            StopCoroutine(MoveRoutine());
+        }
+    }
+}
